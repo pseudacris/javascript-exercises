d7e029ae728be9f77edb8f32cb7cfbcbe5f3a8c2
pseudacris <hyliola@pm.me>
2024-06-09 11:28:40 -0700

    Complete code and all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
+const reverseString = function(gnirts) {
+    let reverso = "";
+    for (var i = gnirts.length - 1; i >= 0; i--) {
+        reverso += gnirts[i];
+    }
+    return reverso;
+}
 
-};
+reverseString('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
