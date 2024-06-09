51b27702226fea59f495769d5ce6423141e19200
pseudacris <hyliola@pm.me>
2024-06-08 21:28:57 -0700

    Complete code and all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(word, reps) {
+    let final = '';
+    if (reps < 0) return 'ERROR';
+    for (i = 0; i < reps; i++) {
+        final += word;
+    }
+    return final;
 };
-
+console.log(repeatString('goodbye', -1))
 // Do not edit below this line
 module.exports = repeatString;
