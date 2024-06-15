25e81f864b84fbfc8db1fde3de6a2f02ea86c907
pseudacris <hyliola@pm.me>
2024-06-15 13:15:41 -0700

    Was on right track with function() but could not figure out without looking @ solution & commented out my original idea

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+// let someArray = [1, 2, 3, 4];
 
+// const removeFromArray = function(value, index, arr) {
+//    if (value === 2) {
+//     arr.splice(index, 1);
+//     return true;
+//    }
+//    return false;
+// }    
+// someArray.filter(removeFromArray());
+
+const removeFromArray = function(array, ...args) {
+   return array.filter(val => !args.includes(val))
+}
 // Do not edit below this line
 module.exports = removeFromArray;
