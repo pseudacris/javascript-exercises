f02f4d1636cf70c2ba45a464a074d7b9b8347fe2
pseudacris <86464203+pseudacris@users.noreply.github.com>
2024-11-08 15:03:15 -0800

    Complete function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,16 @@
 const sumAll = function(min, max) {
     if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
     if (min < 0 || max < 0) return "ERROR";
-    
+    const start = Math.min(min, max);
+    const end = Math.max(min, max);
+    let sum = 0;
+
+    // Loop from start to end, inclusive
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
