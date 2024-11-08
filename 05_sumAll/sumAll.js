e18113af88a20bcf3816c7fe1b167d35652279b5
pseudacris <86464203+pseudacris@users.noreply.github.com>
2024-11-07 16:34:52 -0800

    Add error if statements

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    
 };
 
 // Do not edit below this line
