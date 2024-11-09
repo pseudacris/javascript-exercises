032cdc909bf3dee299889da39ed1431b145555a2
pseudacris <86464203+pseudacris@users.noreply.github.com>
2024-11-08 20:03:01 -0800

    Layout basic outline of function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return "ERROR";
+    if (year < 0) return "ERROR";
+    if (year / 400) {
 
+    } else if () {          //NOT a leap year
+
+    } else if () {          // is a leap year               
+
+    } else () {             // NOT a leap year
+
+    }
 };
 
 // Do not edit below this line
